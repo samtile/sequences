56085f2bf287b29f8f459d395161a22491a0f043
samtile <sammojk@gmail.com>
2025-03-09 23:39:42 +0000

    Added Luca's gap sequence

--- .idea/Main.py
+++ .idea/Main.py
@@ -51,5 +51,35 @@ def sequenceTwo():
     for results in range(iterations):
         print(sequence[results])
 
+# Luca's first gap sequence - to be added to the OEIS
+def sequenceThree():
+    iterations = 1000
+    sequence = [0] * (iterations*2)
+    sequence[0] = 1
+    lastNumber = 1
+    lastN = 0
+    done = 0
+    for x in range(iterations):
+        done = 0
+        for y in range(lastN):
+            if sequence[y] == 0:
+                sequence[y] = sequence[y-1] + sequence[y+1]
+                lastNumber = sequence[y]
+                lastN = y
+                done = 1
+                break
+        if done == 0:
+            lastN = lastN + lastN + lastNumber + 1
+            while 1:
+                if sequence[lastN] != 0:
+                    lastN = lastN + 1
+                else:
+                    break
+            sequence[lastN] = lastNumber + 1
+            lastNumber = lastNumber + 1
+    for results in range(iterations):
+        print(sequence[results])
+
 #sequenceOne()
-sequenceTwo()
+#sequenceTwo()
+sequenceThree()
